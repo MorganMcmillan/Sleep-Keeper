65e1dd876f99ab9644c6e3401a6eb0e482630ca1
Morgan.rs <mcmillan.a.morgan@gmail.com>
2025-01-14 11:34:37 -0700

    Added extra features to the sleep info screen

--- sleep_input_window.py
+++ sleep_input_window.py
@@ -9,13 +9,25 @@ def day_clicked(button: tk.Button, current_date: date, root: tk.Tk, con: sql.Con
         messagebox.showwarning("Future Date Selected", f"You cannot enter your sleep information for a date that's in the future. (Today's date is {today})")
         return
 
-    # TODO: when a day is clicked, open a new window to ask the user for their sleep information
-    # The information will then be recorded in the SQL database
-    # If the day is after today, then the user will be prevented from entering information
+    # TODO: show today's information if the user already entered it
     sub = tk.Toplevel(root)
     sub.geometry("500x500")
+    
+    # Get today's sleep info, if any
+    cur.execute("SELECT slept_at, hours_slept FROM sleep WHERE date = ?;", (str(current_date),))
+    sleep_info = cur.fetchone()
+
+    def update_sleep_info(slept_at, hours_slept):
+        delete_children(info_frame)
+        tk.Label(info_frame, text=f"Went to sleep at: {slept_at}").pack(anchor="nw")
+        tk.Label(info_frame, text=f"Hours of sleep today: {hours_slept}").pack(anchor="nw")
 
     tk.Label(sub, text=f"Date: {current_date}").pack(anchor="nw")
+    info_frame = tk.Frame(sub)
+    if sleep_info:
+        update_sleep_info(sleep_info[0], sleep_info[1])
+    info_frame.pack(anchor="nw")
+
 
     sleep_frame = tk.Frame(sub)
     tk.Label(sleep_frame, text="Enter the time you fell asleep at (Ex: 11:00)").grid(row=0, column=0)
@@ -40,19 +52,19 @@ def day_clicked(button: tk.Button, current_date: date, root: tk.Tk, con: sql.Con
 
     def record_sleep_info():
         try:
-            time_slept_at = slept_at.get() + am_pm.get()[listbox.curselection()[0]]
+            time_slept_at = slept_at.get() + " " + am_pm.get()[listbox.curselection()[0]]
         except IndexError:
             time_slept_at = "12:00 A.M."
-        
-        cur.execute("INSERT OR REPLACE INTO sleep VALUES (?, ?, ?)", (str(current_date), time_slept_at, hours_slept.get()))
+        cur.execute("INSERT OR REPLACE INTO sleep VALUES (?, ?, ?);", (str(current_date), time_slept_at, hours_slept.get()))
         con.commit()
-
         update_todays_color(button, hours_slept.get(), 8)
+        update_sleep_info(time_slept_at, hours_slept.get())
 
     def delete_sleep_info():
-        cur.execute("DELETE FROM sleep WHERE date = ?", (str(current_date),))
+        cur.execute("DELETE FROM sleep WHERE date = ?;", (str(current_date),))
         con.commit()
         update_todays_color(button, None, 8)
+        delete_children(info_frame)
 
     button_group = tk.Frame(sub)
     record_btn = tk.Button(button_group, text="Record", command=record_sleep_info)
@@ -71,7 +83,9 @@ def get_sleep_color(hours_slept, hours_of_sleep_needed):
         return ("white", "black")
 
     delta_slept = hours_slept - hours_of_sleep_needed
-    if delta_slept <= -2.0:
+    if delta_slept <= -3.0:
+        return ("VioletRed4", "black")
+    elif delta_slept <= -2.0:
         return ("red2", "black")
     elif delta_slept <= -1.0:
         return ("orange", "black")
@@ -81,3 +95,8 @@ def get_sleep_color(hours_slept, hours_of_sleep_needed):
         return ("green2", "black")
     else:
         return ("blue2", "white")
+    
+def delete_children(widget):
+    children = [v for v in widget.children.values()]
+    for child in children:
+        child.destroy()
